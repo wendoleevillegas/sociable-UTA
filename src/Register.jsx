735ab5a04cf170a6728f879970e7108f7d78e6d9
Wendolee <167564740+wendoughlee@users.noreply.github.com>
2025-08-06 03:09:02 +0000

    fix register.jsx and register.module.css

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,3 +1,5 @@
+import styles from './Register.module.css';
+
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 const REGISTER_URL = '/register';
