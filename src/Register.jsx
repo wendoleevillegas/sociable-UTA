60fb1e21ce1903e6ee4ff42b6e4250340eb93da3
Wendolee <167564740+wendoughlee@users.noreply.github.com>
2025-08-06 03:16:15 +0000

    fix register.jsx

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,4 +1,9 @@
 import styles from './Register.module.css';
+import { useRef, useState, useEffect } from "react";
+import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from './api/axios';
+
 
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
