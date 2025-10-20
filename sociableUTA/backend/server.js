b36104047c3433f3ee1f3698a6f6af51c55fc93d
Wendolee <167564740+wendoughlee@users.noreply.github.com>
2025-10-20 02:44:18 +0000

    adding vercel URL to corsheader

--- sociableUTA/backend/server.js
+++ sociableUTA/backend/server.js
@@ -16,7 +16,8 @@ const corsOptions = {
     origin: [
         'http://localhost:3000',
         'http://localhost:5173',
-        'http://127.0.0.1:5173'
+        'http://127.0.0.1:5173',
+        'https://sociableuta.vercel.app'
     ],
     credentials: true
 };
